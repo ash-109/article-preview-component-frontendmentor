142e295ab807d8cd3dd50990ddd1d688e4c2607b
Ashvin Antyah <a.antyah@gmail.com>
2024-05-22 08:47:47 +0200

    update html css js
    -add the share icon for mobile version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const shareIcon = document.getElementById('shareIcon');
 const sharePopUp = document.getElementById('share-pop-up');
 const shareIconLogo = document.getElementById('share_icon_logo')
+const shareMobile=document.getElementById('share_icon_mobile');
 
 shareIcon.addEventListener('click', () => {
    sharePopUp.classList.toggle('visible');
@@ -14,3 +15,8 @@ shareIconLogo.style.fill='#fff'
 shareIconLogo.style.fill='#6e8098'
     }
 });
+shareMobile.addEventListener('click', ()=>{
+    sharePopUp.classList.toggle('visible');
+    sharePopUp.classList.add('hidden');
+  
+})
