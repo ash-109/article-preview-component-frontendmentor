d8622170db13dc94f1de9bc4ceebc19021ccf949
Ashvin Antyah <a.antyah@gmail.com>
2024-05-28 10:39:36 +0200

    update js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const shareIcon = document.getElementById('shareIcon');
-const sharePopUp = document.getElementById('share-pop-up');
-const shareIconLogo = document.getElementById('share_icon_logo')
-const shareMobile=document.getElementById('share_icon_mobile');
+const shareIcon = document.getElementById("shareIcon");
+const sharePopUp = document.getElementById("share-pop-up");
+const shareIconLogo = document.getElementById("share_icon_logo");
+const shareMobile = document.getElementById("share_icon_mobile");
 
-shareIcon.addEventListener('click', () => {
-   sharePopUp.classList.toggle('visible');
-    if (sharePopUp.classList.contains('visible')) {
-        sharePopUp.classList.remove('hidden');
-shareIcon.style.backgroundColor='#6e8098'
-shareIconLogo.style.fill='#fff'
-    } else {
-        sharePopUp.classList.add('hidden');
-        shareIcon.style.backgroundColor='#ecf2f8'
-shareIconLogo.style.fill='#6e8098'
-    }
+shareIcon.addEventListener("click", () => {
+  sharePopUp.classList.toggle("visible");
+  if (sharePopUp.classList.contains("visible")) {
+    sharePopUp.classList.remove("hidden");
+    shareIcon.style.backgroundColor = "#6e8098";
+    shareIconLogo.style.fill = "#fff";
+  } else {
+    sharePopUp.classList.add("hidden");
+    shareIcon.style.backgroundColor = "#ecf2f8";
+    shareIconLogo.style.fill = "#6e8098";
+  }
 });
-shareMobile.addEventListener('click', ()=>{
-    sharePopUp.classList.toggle('visible');
-    sharePopUp.classList.add('hidden');
-  
-})
+
+
+//For Mobile devices
+
+shareMobile.addEventListener("click", () => {
+  sharePopUp.classList.toggle("visible");
+  sharePopUp.classList.add("hidden");
+});
